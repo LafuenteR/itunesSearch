2c4088182a3db8f14121d2853a8bef3be8c3e9a5
Ruel Lafuente <ruellafuente03@gmail.com>
2021-06-13 03:27:11 +0800

    added favorites

--- Itunes Search/FavoriteController.swift
+++ Itunes Search/FavoriteController.swift
@@ -6,24 +6,36 @@
 //
 
 import UIKit
+import RealmSwift
 
-class FavoriteController: UIViewController {
+class FavoriteController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var favoriteTableView: UITableView!
+    var favorites: Results<TrackModel>?
+    var results = [result]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        favoriteTableView.delegate = self
+        favoriteTableView.dataSource = self
+        favoriteTableView.register(UINib(nibName: "TrackCell", bundle: nil), forCellReuseIdentifier: "TrackCell")
         // Do any additional setup after loading the view.
+//        let config = Realm.Configuration(objectTypes: [TrackModel.self])
+//        let realm = try! Realm(configuration: config)
+        
+        let hey = try! Realm().objects(TrackModel.self).filter("isFavorite == \(true)")
+        print("dbgbf",hey.count)
+        favorites = hey
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites!.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = favoriteTableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
+        cell.update(track: favorites![indexPath.row])
+        return cell
+    }
 
 }
---
+++ Itunes Search/TrackCell.swift
@@ -0,0 +1,35 @@
+//
+//  TrackCell.swift
+//  Itunes Search
+//
+//  Created by Ruel Lafuente on 6/13/21.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+    @IBOutlet weak var artworkUrl30: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(track: TrackModel) {
+        trackNameLabel.text = track.trackName 
+        priceLabel.text = "$ \(track.trackPrice )"
+        let url = URL(string: track.artworkUrl30 )
+        artworkUrl30.contentMode = .scaleAspectFill
+        artworkUrl30.kf.setImage(with: url)
+    }
+    
+}
